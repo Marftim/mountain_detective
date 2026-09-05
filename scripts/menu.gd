cdc73a77e1c670beaddca317d98f037012ce85f2
Marftim <timofei.marfetkin@yandex.ru>
2026-09-05 18:54:47 +0300

    Fix

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,4 +6,5 @@ func _on_exit_pressed() -> void:
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	#get_tree().change_scene_to_file("res://scenes/level.tscn")
+	pass
