4610a33a790327cb95bd87f58ea09901386e35ee
Marftim <timofei.marfetkin@yandex.ru>
2026-09-05 19:37:22 +0300

    Fix

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -8,3 +8,7 @@ func _on_exit_pressed() -> void:
 func _on_play_pressed() -> void:
 	#get_tree().change_scene_to_file("res://scenes/level.tscn")
 	pass
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
